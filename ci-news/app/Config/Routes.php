a0bd8df6a164986677c043b86c28cc50248547bc
panduprasetyap <prasetya038@gmail.com>
2022-03-03 14:26:43 +0700

    update CRUD

--- ci-news/app/Config/Routes.php
+++ ci-news/app/Config/Routes.php
@@ -38,6 +38,14 @@ $routes->get('/faqs', 'Page::faqs');
 $routes->get('/news', 'News::index');
 $routes->get('/news/(:any)', 'News::viewNews/$1');
 
+$routes->group('admin', function ($routes) {
+    $routes->get('news', 'NewsAdmin::index');
+    $routes->get('news/(:segment)/preview', 'NewsAdmin::preview/$1');
+    $routes->add('news/new', 'NewsAdmin::create');
+    $routes->add('news/(:segment)/edit', 'NewsAdmin::edit/$1');
+    $routes->get('news/(:segment)/delete', 'NewsAdmin::delete/$1');
+});
+
 /*
  * --------------------------------------------------------------------
  * Additional Routing
